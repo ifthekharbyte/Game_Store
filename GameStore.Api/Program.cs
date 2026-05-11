a5a56d5f0e3e04cf4fb430be4f6ee422c0944b89
Iftekhar Iqbal Enan <ifthekharenan6@gmail.com>
2026-05-11 17:57:34 +0600

    Database connected

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,8 +1,11 @@
+using GameStore.Api.Data;
 using GameStore.Api.Dtos;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddValidation();
+
+builder.Services.AddSqlite<GameStoreContext>("Data Source=gamestore.db");
 
 var app = builder.Build();
 
--- GameStore.Api/obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ea6dc42e4b3781e914277a54ec93e970934cfea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d690292b1b6badcbd04ce94885509f39022edc6")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
