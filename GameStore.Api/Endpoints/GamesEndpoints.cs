02ffb6109f4dadb960ceb994c1e1ba2e52d695b9
Iftekhar Iqbal Enan <ifthekharenan6@gmail.com>
2026-05-14 16:34:11 +0600

    Deleted Hardcoded list

--- GameStore.Api/Endpoints/GamesEndpoints.cs
+++ GameStore.Api/Endpoints/GamesEndpoints.cs
@@ -9,11 +9,7 @@ public static class GamesEndpoints
     {
 
         const string GetGameEndpoint = "GetGame";
-        private static readonly List<GameSummaryDto> games = [
-            new (1, "Stardew Valley", "Simulation", 14.99M, new DateOnly(2016, 2, 26)),
-            new (2, "Hades", "Rogue-like", 24.99M, new DateOnly(2020, 9, 17)),
-            new (3, "The Witcher 3: Wild Hunt", "Action RPG", 39.99M, new DateOnly(2015, 5, 19))    
-        ];
+
 
          public static void MapGamesEndpoints(this WebApplication app)
         {
